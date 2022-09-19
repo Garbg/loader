70577ed7dcceee02e59c0b0f0a505e040e64778d
Garbg <83542885+CloudyWhale@users.noreply.github.com>
2022-09-18 20:31:16 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function hideload(){
+    console.log("Loaded");
+    //Uncomment the line below if you want the loader to hide on page load
+    //document.getElementsByClassName("loader")[0].style.display =  "none";
+}
+
+window.onload = hideload
